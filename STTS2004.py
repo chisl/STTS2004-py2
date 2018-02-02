975a73bac5dfd1aea0d0d619d6591943367b2e3d
chisl <info@chisl.io>
2018-02-02 20:29:28 +0100

    update

--- STTS2004.py
+++ STTS2004.py
@@ -61,13 +61,53 @@ class STTS2004_Base:
 	# Bits reserved_0
 	# These values must be set to '0'. 
 	# Bits EVSD
+	# nEVENT behavior upon shutdown (default) 
 	# Bits TMOUT
+	# bus timeout support 
 	# Bits VHV
+	# (VHV) high voltage support for A0 (pin 1) 
 	# Bits TRES1
+	# Temperature resolution 
 	# Bits Wider_range
 	# Bits Higher_precision
 	# Bits Alarm_and_critical_trips
 	# Register Configuration
+	# The 16-bit configuration register stores various configuration modes that are used to set up
+	#       the sensor registers and configure according to application and JEDEC requirements (see
+	#       Table 9 on page 21 and Table 10 on page 21).
+	#       
+	#       The temperature sensor configuration register holds the control and status bits of the
+	#       EVENT pin as well as general hysteresis on all limits. To avoid glitches on the EVENT
+	#       output pin, users should disable EVENT or CRITICAL functions prior to programming or
+	#       changing other device configuration settings.
+	#       
+	#       4.2.1 Event thresholds
+	#       All event thresholds use hysteresis as programmed in register address 0x01 (bits 10
+	#       through 9) to be set when they de-assert.
+	#       
+	#       4.2.2 Interrupt mode
+	#       The interrupt mode allows an event to occur where software may write a '1' to the clear
+	#       event bit (bit 5) to de-assert the event Interrupt output until the next trigger condition occurs.
+	#       
+	#       4.2.3 Comparator mode
+	#       The comparator mode enables the device to be used as a thermostat. READs and WRITEs
+	#       on the device registers will not affect the event output in comparator mode. The event signal
+	#       will remain asserted until temperature drops outside the range or is re-programmed to make
+	#       the current temperature “out of range”.
+	#       
+	#       4.2.4 Shutdown mode
+	#       The STTS2004 features a shutdown mode which disables all power-consuming activities
+	#       (e.g. temperature sampling operations), and leaves the serial interface active. This is
+	#       selected by setting the shutdown bit (bit 8) to '1'. In this mode, the devices consume the
+	#       minimum current (ISHDN), as shown in Table 30 on page 44.
+	#       NOTE: Bit 8 cannot be set to '1' while bits 6 and 7 (the lock bits) are set to '1'.
+	#       The device may be enabled for continuous operation by clearing bit 8 to '0'. In shutdown
+	#       mode, all registers may be read or written to. Power recycling will also clear this bit and
+	#       return the device to continuous mode as well.
+	#       If the device is shut down while the EVENT pin is asserted, then the Event output will be deasserted
+	#       during shutdown. It will remain de-asserted until the device is enabled for normal
+	#       operation. Once the device is enabled, it takes tCONV before the device can re-assert the
+	#       Event output. 
 	
 	
 	def setConfiguration(self, val):
@@ -78,7 +118,33 @@ class STTS2004_Base:
 		"""Get register Configuration"""
 		return self.read(REG.Configuration, 16)
 	
-	# Bits Configuration
+	# Bits reserved_0
+	# These bits will always read ‘0’ and writing to them will have no effect. For future
+	#           compatibility, all RFU bits must be programmed as ‘0’ 
+	
+	# Bits Hysteresis
+	# Bits Shutdown_mode
+	# Bits Critical_lock_bit
+	# Bits Alarm_lock_bit
+	# Bits Clear_event
+	# Writing to this register has no effect on overall device functioning in comparator mode. 
+	#           When read, this bit will always return a logic '0' result. 
+	
+	# Bits Output_status
+	# NOTE: The actual incident causing the event can be determined from the read temperature
+	#         register. Interrupt events can be cleared by writing to the clear event bit (writing to
+	#         this bit will have no effect on overall device functioning). 
+	
+	# Bits Event_output_control
+	# Bits Critical_event_only
+	# Bits Event_polarity
+	# The event polarity bit controls the active state of the EVENT pin. The EVENT pin is driven to this state
+	#           when it is asserted. 
+	#           NOTE: As this device is used in DIMM (memory modules) applications, it is strongly recommended
+	#           that only the active-low polarity (default) is used. This will provide full compatibility
+	#           with the STTS2002. This is the recommended configuration for the STTS2004. 
+	
+	# Bits Event_mode
 	# Register Alarm_Upper
 	# Alarm temperature upper boundary trip 
 	
